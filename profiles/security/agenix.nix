37f702227c7ed285e9bbdf3b122bf7a6302a4052
afsc19 <71138696+afsc19@users.noreply.github.com>
2025-11-18 02:45:35 +0000

    feat: agenix basic keys + hardware security key

--- profiles/security/agenix.nix
+++ profiles/security/agenix.nix
@@ -14,5 +14,15 @@ in
     inputs.agenix.nixosModules.default
   ];
 
-  age.identityPaths = [ "${systemRoot}/etc/ssh/ssh_host_ed25519_key" ];
+  age.identityPaths = [
+    "${systemRoot}/etc/ssh/ssh_host_ed25519_key" 
+    "/etc/age/fido2-hmac-magic.txt"
+  ];
+
+  environment.etc."age/fido2-hmac-magic.txt".text = "AGE-PLUGIN-FIDO2-HMAC-1VE5KGMEJ945X6CTRM2TF76";
+
+  environment.systemPackages = with pkgs; [
+    age
+    age-plugin-fido2-hmac
+  ];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 # Agenix secrets, heavily inspired on Diogotcorreia's dotfiles.
 let
   zenSystem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHh9INLI4sUow/VZaBoZGwdlr3ZoYa8/j58ahzSK1LPE";
+  thetis = "age1fido2-hmac1qqpgqkdh6zc0q6cw5zwm9neeke0wgpj5pz7xrj8ewrl6hkav86ht50sp43tlvlvtwnjzesmwp7uyrf4f03auc9sq3psghzxem3yjplld4mmn6mj3klccuyaduqrgwvekfakam89f5qwsgag2utsa3vf32exe0vv756t4l9ym3078e257gju9eev4s4g9av4q5x5tvrc5r6l97aggn94cqnh40kgyn72fcrwg0p535wrx7898084tau2tq8stxg3g9c9wsvrlzwd";
 
   personalSystems = [
     zenSystem
+    thetis
   ];
   serverSystems = [
     # TODO add oracle1
@@ -34,6 +36,10 @@ let
   mkSecrets = systems: builtins.listToAttrs (flatten systems);
 in
 mkSecrets [
+
+  # Add secrets on another distro using: nix-shell -E 'let pkgs = import <nixpkgs> {}; src = builtins.fetchTarball "https://github.com/ryantm/agenix/archive/main.tar.gz"; agenix = pkgs.callPackage "${src}/pkgs/agenix.nix" {}; in pkgs.mkShell { packages = [ agenix ]; }'
+
+
   (mkSystem null allSystems [
     # TODO add keys for all
   ])
@@ -45,7 +51,7 @@ mkSecrets [
   ])
 
   (mkSystem "zen"
-    [ zenSystem ]
+    [ zenSystem thetis ]
     [
       # TODO add secrets
     ]
