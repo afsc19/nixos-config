a1c6997498d3409308457dc03833a2465f9e01e2
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-01-29 01:53:00 -0100

    feat: Use power-profiles-daemon if using gnome, TLP otherwise

--- modules/laptop/battery.nix
+++ modules/laptop/battery.nix
@@ -2,19 +2,20 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.laptop.battery;
-
+  batteryPowered = config.my.hardware.batteryPowered;
+  isGnome = config.modules.graphical.gnome.enable;
 in
 {
   options.modules.laptop.battery.enable = mkEnableOption "LAPTOP BATTERY";
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable && batteryPowered) {
 
     # System76 scheduler (improves CPU scheduling profiles)
     services.system76-scheduler.enable = true;
     services.system76-scheduler.settings.cfsProfiles.enable = true;
 
-    # Enable TLP (better than gnomes internal power manager)
+    # Enable TLP if we're NOT using GNOME
     services.tlp = {
-      enable = true;
+      enable = !isGnome;
       settings = {
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
@@ -27,9 +28,8 @@ in
       };
     };
 
-    # TLP replaces GNOME's power-profiles-daemon
-    # mkForce wins over other modules to disable it. (conflicts)
-    services.power-profiles-daemon.enable = lib.mkForce false;
+    # Enable power-profiles-daemon if we're using GNOME
+    services.power-profiles-daemon.enable = lib.mkForce isGnome;
 
     # Enable powertop
     powerManagement.powertop.enable = true;
