31ec588efdd22e0f3436d0fca961e9387615be68
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-04 23:51:07 -0100

    feat: boot animation

--- profiles/security/securegrub.nix
+++ profiles/security/securegrub.nix
@@ -3,6 +3,27 @@ let
   secureBootDir = "/var/lib/sbctl/keys"; # sbctl default; created automatically by sbctl create-keys
 in
 {
+  # Plymouth:
+  boot.plymouth = {
+    enable = true;
+    theme = "bgrt"; # "bgrt" uses the UEFI Vendor Logo (ACPI BGRT) + Spinner
+  };
+
+  # Enable systemd in initrd. This is required for the modern graphical 
+  # LUKS unlock prompt to integrate correctly with Plymouth.
+  boot.initrd.systemd.enable = true;
+
+  # Allow Plymouth to show the animation
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
+  boot.consoleLogLevel = 0;
 
   # Use sbctl verify, sbctl status, to check if it's working.
   boot.loader.systemd-boot.enable = false;
