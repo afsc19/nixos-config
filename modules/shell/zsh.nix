687b8ff35dcce32fc4bde4ced69cfe47be7d976d
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-06 18:34:46 -0100

    fix: deprecation

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -46,7 +46,7 @@ in
         };
 
         # Disable beep when no file is found, per example.
-        initExtra = ''
+        initContent = ''
           # unsetopt beep
           bindkey "^[[1;5C" forward-word # Ctrl + Right Arrow
           bindkey "^[[1;5D" backward-word # Ctrl + Left Arrow
