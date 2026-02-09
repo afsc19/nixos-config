c3a50795694bd9c44ff22a4e90e76bee7c3bd7e2
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-09 17:14:17 +0000

    feat: torrenting

---
+++ modules/graphical/torrenting.nix
@@ -0,0 +1,20 @@
+# Discord configuration
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.graphical.thunderbird;
+in
+{
+  options.modules.graphical.thunderbird.enable = mkEnableOption "Thunderbird";
+
+  config = mkIf cfg.enable {
+    hm.home.packages = with pkgs; [
+      qbittorrent
+    ];
+  };
+}
