e98c7af04b8348361886ebb23944ea0d228fb403
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-13 02:31:48 +0000

    chore: removing trash

--- profiles/laptop-virtualization.nix
+++ profiles/laptop-virtualization.nix
@@ -4,15 +4,10 @@
   ...
 }:
 {
-
   modules.virtualization = {
     docker.enable = true;
     distrobox.enable = true;
     kvm.enable = true; # qemu_kvm + libvirtd
     vmware.enable = true;
   };
-
-  # System packages
-  environment.systemPackages = with pkgs; [
-  ];
 }
