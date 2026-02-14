0f951f27edc1893b4cefabe4b29331a7fbb03c4f
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-14 01:18:23 +0000

    feat: cybersec pwn

---
+++ profiles/cybersec/pwn.nix
@@ -0,0 +1,20 @@
+# Pwn utils
+{
+  hm,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  pwndbg = inputs.pwndbg.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in
+{
+  hm.home.packages = with pkgs.unstable; [
+    gef # Enhanced gdb
+    pwndbg
+  ]
+
+  hm.home.shellAliases = {
+    gdb = "${pwndbg}/bin/pwndbg";
+  };
+}
