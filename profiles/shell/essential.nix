4c6c76f4ce76ff16073ab83c669a759fcc388f74
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-02-26 14:49:07 +0000

    feat: tcpdump

--- profiles/shell/essential.nix
+++ profiles/shell/essential.nix
@@ -55,7 +55,8 @@
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc # it is a calculator for the IPv4/v6 addresses
-    iw
+    iw    # Wireless interfaces
+    tcpdump # To sniff tcp connections
 
     # Monitoring tools
     btop # replacement of htop/nmon
