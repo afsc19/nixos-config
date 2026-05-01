edd4abcfc818f85a36e4a730b151101ba85dcef5
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-05-02 00:33:52 +0100

    fix: binfmt emulators for docker

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -9,20 +9,6 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.virtualization.docker;
-
-  qemu-x86_64-static = pkgs.stdenv.mkDerivation {
-    name = "qemu-x86_64-static";
-    src = pkgs.fetchurl {
-      url = "https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-x86_64-static";
-      sha256 = "sha256-cTL/05rvccJtM0TMDH3/xTDhDj5yDFjIJ5qX72/dd4Q=";
-    };
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/qemu-x86_64
-      chmod +x $out/bin/qemu-x86_64
-    '';
-  };
 in
 {
   options.modules.virtualization.docker = {
@@ -44,19 +30,17 @@ in
       docker-buildx
     ];
     
-    boot.binfmt = mkIf (cfg.useVirtualization && pkgs.stdenv.hostPlatform.isAarch64) {
-
-      registrations = {
-        # General pwn challenges
-        x86_64-linux = {
-          interpreter = "${qemu-x86_64-static}/bin/qemu-x86_64";
-          fixBinary = true;
-          wrapInterpreterInShell = false;
-          magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
-          mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
-        };
+    systemd.services.docker-binfmt = mkIf (cfg.useVirtualization && pkgs.stdenv.hostPlatform.isAarch64) {
+      description = "Register binfmt emulators for Docker";
+      after = [ "network.target" "docker.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.docker}/bin/docker run --privileged --rm tonistiigi/binfmt --install all";
+        RemainAfterExit = true;
       };
     };
 
   };
 }
+
