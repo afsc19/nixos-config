460f0c8fea16ef75629e1f7bdeab245cf6352dc8
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-05-02 00:02:51 +0100

    feat: lightweight static qemu-user for binfmt

--- modules/virtualization/docker.nix
+++ modules/virtualization/docker.nix
@@ -9,9 +9,22 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.virtualization.docker;
+
+  # Lightweight static qemu-user
+  qemu-user-static = pkgs.qemu-user.overrideAttrs (oldAttrs: {
+    configureFlags = (oldAttrs.configureFlags or []) ++ [
+      "--static"
+      "--disable-gnutls"
+      "--disable-nettle"
+      "--disable-gcrypt"
+    ];
+  });
 in
 {
-  options.modules.virtualization.docker.enable = mkEnableOption "docker";
+  options.modules.virtualization.docker = {
+    enable = mkEnableOption "docker";
+    useVirtualization = mkEnableOption "Use QEMU user virtualization to run x86_64 binaries";
+  };
 
   config = mkIf cfg.enable {
     virtualisation.docker = {
@@ -26,5 +39,22 @@ in
     environment.systemPackages = with pkgs; [
       docker-buildx
     ];
+    
+    boot.binfmt = mkIf (cfg.useVirtualization && pkgs.stdenv.hostPlatform.isAarch64) {
+      # Run other arch binaries using QEMU
+      emulatedSystems = [ "x86_64-linux" ];
+
+      registrations = {
+        # General pwn challenges
+        x86_64-linux = {
+          interpreter = "${qemu-user-static}/bin/qemu-x86_64";
+          fixBinary = true;
+          wrapInterpreterInShell = false;
+          magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+          mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+        };
+      };
+    };
+
   };
 }
