db9f0aa24c49dcf3fba7693ba184be4203d93abf
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-06-23 21:20:30 +0100

    fix: audio: bluetooth fixes

--- profiles/audio.nix
+++ profiles/audio.nix
@@ -18,6 +18,15 @@
     #isDefault
     #wireplumber.enable= true;
 
+    # bluetooth fixes
+    wireplumber.extraConfig."10-bluez" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-hw-volume" = true;
+      };
+    };
+
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     # media-session.enable = true;
