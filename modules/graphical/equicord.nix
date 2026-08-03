4d7b058801ac8b20dc293303ebdedceeeef719f7
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-08-03 21:13:38 +0100

    chore: discord plugin

--- modules/graphical/equicord.nix
+++ modules/graphical/equicord.nix
@@ -44,6 +44,7 @@ in
           disableCallIdle.enable = true;
           iRememberYou.enable = true;
           messageLoggerEnhanced.enable = true;
+          permissionsViewer.enable = true;
         };
       };
     };
