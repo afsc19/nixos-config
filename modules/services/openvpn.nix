f378a3e3966ed49b70fea67357dbf4254dcb81a3
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-08-04 05:53:40 +0100

    feat: tecnico openvpn declaration

--- modules/services/openvpn.nix
+++ modules/services/openvpn.nix
@@ -8,6 +8,19 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.services.openvpn;
+
+  ovpnFile = ../../config/vpns/tecnico.ovpn;
+  ovpn = builtins.readFile ovpnFile;
+
+  # Extract a PEM block delimited by <tag>...</tag> from an .ovpn file.
+  extract = tag: let
+    afterOpen   = builtins.elemAt (builtins.split "<${tag}>" ovpn) 2;
+    beforeClose = builtins.elemAt (builtins.split "</${tag}>" afterOpen) 0;
+    trimmed     = builtins.match "[[:space:]]*(.*)[[:space:]]*" beforeClose;
+  in builtins.elemAt trimmed 0;
+
+  tecnicoCert = pkgs.writeText "tecnico-ca.pem" (extract "ca");
+  tecnicoTa   = pkgs.writeText "tecnico-tls-auth.pem" (extract "tls-auth");
 in
 {
   options.modules.services.openvpn.enable = mkEnableOption "OpenVPN";
@@ -19,8 +32,45 @@ in
       networkmanager-openvpn
     ];
 
-    # This is often the missing piece on NixOS for NetworkManager to actually
-    # use the plugin binaries installed in the system environment.
+    # tecnico VPN
+    age.secrets.tecnicoVpnPassword.file = ../../secrets/personal/tecnicoVpnPassword.age;
+
+    networking.networkmanager.ensureProfiles = {
+      environmentFiles = [ config.age.secrets.tecnicoVpnPassword.path ];
+      profiles.tecnico = {
+        connection = {
+          id = "tecnico";
+          type = "vpn";
+        };
+        vpn = {
+          service-type = "org.freedesktop.NetworkManager.openvpn";
+          connection-type = "password";
+          auth = "SHA256";
+          cipher = "AES-256-CBC";
+          dev = "tun";
+          password-flags = "0";
+          remote = "vpn.tecnico.ulisboa.pt";
+          remote-cert-tls = "server";
+          tls-version-min = "1.2";
+          ca = "${tecnicoCert}";
+          ta = "${tecnicoTa}";
+          ta-dir = "0";
+          username = "ist1114254@tecnico.ulisboa.pt";
+        };
+        vpn-secrets = {
+          password = "\${TECNICO_VPN_PASSWORD}";
+        };
+        ipv4 = {
+          method = "auto";
+        };
+        ipv6 = {
+          method = "auto";
+        };
+      };
+    };
+
+    # This is the missing piece that makes NetworkManager's VPN plugin
+    # actually find its helper binaries.
     networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
   };
 }
