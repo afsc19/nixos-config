dab5a4ebe0ba84fd64ce6e1389d06ea419697d0f
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-08-25 20:56:42 +0100

    fix: openrazer to unstable

--- modules/hardware/razer.nix
+++ modules/hardware/razer.nix
@@ -8,6 +8,17 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.hardware.razer;
+
+  # FIXME quick fix since kernel 7.2.0 removed strncpy from the kernel but 3.12.3 still required it
+  openrazer' = {
+    version = "3.12.4";
+    src = pkgs.fetchFromGitHub {
+      owner = "openrazer";
+      repo = "openrazer";
+      tag = "v3.12.4";
+      hash = "sha256-WgDYs0ehnzWlX/wvfur0UhFLbZv7jZ6FMybqDaFDuLg=";
+    };
+  };
 in
 {
   options.modules.hardware.razer.enable = mkEnableOption "razer";
@@ -16,6 +27,8 @@ in
     hardware.openrazer = {
       enable = true;
       users = [ user ];
+      packages.kernel = config.boot.kernelPackages.openrazer.overrideAttrs (_: openrazer');
+      packages.daemon = pkgs.python3Packages.openrazer-daemon.overrideAttrs (_: openrazer');
     };
 
     environment.systemPackages = with pkgs; [
