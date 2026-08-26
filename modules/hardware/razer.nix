97d8220c8fa7ed7e38343238bffbd04077be5d5a
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-08-27 00:10:37 +0100

    fix: openrazer: kernel+daemon to unstable

--- modules/hardware/razer.nix
+++ modules/hardware/razer.nix
@@ -9,16 +9,6 @@ let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.hardware.razer;
 
-  # FIXME quick fix since kernel 7.2.0 removed strncpy from the kernel but 3.12.3 still required it
-  openrazer' = {
-    version = "3.12.4";
-    src = pkgs.fetchFromGitHub {
-      owner = "openrazer";
-      repo = "openrazer";
-      tag = "v3.12.4";
-      hash = "sha256-WgDYs0ehnzWlX/wvfur0UhFLbZv7jZ6FMybqDaFDuLg=";
-    };
-  };
 in
 {
   options.modules.hardware.razer.enable = mkEnableOption "razer";
@@ -27,8 +17,14 @@ in
     hardware.openrazer = {
       enable = true;
       users = [ user ];
-      packages.kernel = config.boot.kernelPackages.openrazer.overrideAttrs (_: openrazer');
-      packages.daemon = pkgs.python3Packages.openrazer-daemon.overrideAttrs (_: openrazer');
+      packages.kernel = config.boot.kernelPackages.openrazer.overrideAttrs (_: {
+        version = "unstable";
+        src = pkgs.unstable.linuxPackages.openrazer.src;
+      });
+      packages.daemon = pkgs.python3Packages.openrazer-daemon.overrideAttrs (_: {
+        version = "unstable";
+        src = pkgs.unstable.openrazer-daemon.src;
+      });
     };
 
     environment.systemPackages = with pkgs; [
