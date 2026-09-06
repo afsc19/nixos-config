2af133fa78ad0d7bf1c0eac770305c398b6dc636
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-09-06 23:02:11 +0100

    fix: binja: logo hash

--- profiles/cybersec/rev/binary-ninja.nix
+++ profiles/cybersec/rev/binary-ninja.nix
@@ -13,6 +13,10 @@ let
   basePath = "${config.my.softwareDirectory}/binja";
   binjaZip = /. + "${basePath}/binaryninja_linux_stable_personal.zip";
   kgPath = /. + "${basePath}/keygen.py";
+  correctIcon = pkgs.fetchurl {
+    url = "https://docs.binary.ninja/img/logo.png";
+    hash = "sha256-waXgwz9lSJ2zPahtqCP+ZdL5Ac6RZ4/pnz7iB4bTs4c=";
+  };
 in
 {
   assertions = [
@@ -40,6 +44,34 @@ in
           autoPatchelfIgnoreMissingDeps = [
             "libQt6WaylandEglClientHwIntegration.so.6"
           ];
+
+          # FIXME temp workaround for logo hash mismatch
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            mkdir -p $out/opt/binaryninja
+            mkdir -p $out/share/pixmaps
+            cp -r * $out/opt/binaryninja
+            find $out/opt/binaryninja \
+              -type f \
+              -name '*.so' -or -name '*.so.*' \
+              -not -name '*.bntl' \
+              -not -name 'libbinaryninjacore.so.*' \
+              -not -name 'libbinaryninjaui.so.*' \
+              -not -name 'liblldb.so.*' \
+              -not -name 'libshiboken6.abi*.so.*' \
+              -not -name 'libpyside6.abi*.so.*' \
+              -delete
+            cp ${correctIcon} $out/share/pixmaps/binaryninja.png
+            chmod +x $out/opt/binaryninja/binaryninja
+            buildPythonPath "$pythonDeps"
+            makeWrapper $out/opt/binaryninja/binaryninja $out/bin/binaryninja \
+              --prefix PYTHONPATH : "$program_PYTHONPATH" \
+              "''${qtWrapperArgs[@]}"
+
+            runHook postInstall
+          '';
 
           # Use Python 3.12 for Sidekick plugin compatibility (requires 3.10-3.12)
           nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
