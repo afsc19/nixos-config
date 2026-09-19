c7e3565e9185d2e9f435ffba8efc2c7d9cb315e7
afsc19 <71138696+afsc19@users.noreply.github.com>
2026-09-19 01:55:38 +0100

    fix: opencode:

---
+++ overlays/opencode/default.nix
@@ -0,0 +1,54 @@
+{ ... }:
+final: prev: {
+  opencode =
+    let
+      version = "1.18.31";
+
+      src = prev.fetchurl {
+        url = "https://github.com/anomalyco/opencode/releases/download/v${version}/opencode-linux-${
+          if prev.stdenv.hostPlatform.isx86_64 then "x64" else "arm64"
+        }.tar.gz";
+        sha256 =
+          if prev.stdenv.hostPlatform.isx86_64 then
+            "e9312be75ed803b7415fc2aeabda1f4fe938912a39673762dc0c38c0e11ebde4"
+          else
+            "d4e332f46b227448582c0d9fc75f6f826dfe95c9f751bc2011fc4d937a042be6";
+      };
+
+      unwrapped = prev.stdenv.mkDerivation {
+        pname = "opencode-unwrapped";
+        inherit version src;
+
+        sourceRoot = ".";
+        dontBuild = true;
+        dontFixup = true; # patchelf/strip messes the bin
+
+        # copy pasted
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          cp opencode $out/bin/.opencode-unwrapped
+          chmod +x $out/bin/.opencode-unwrapped
+          runHook postInstall
+        '';
+      };
+    in
+    prev.buildFHSEnv {
+      # copy pasted
+      name = "opencode";
+      targetPkgs = pkgs: [
+        pkgs.stdenv.cc.cc.lib
+        pkgs.zlib
+      ];
+      runScript = "${unwrapped}/bin/.opencode-unwrapped";
+      meta = with prev.lib; {
+        description = "AI coding agent built for the terminal";
+        mainProgram = "opencode";
+      };
+    };
+
+  # im using unstable
+  unstable = prev.unstable // {
+    inherit (final) opencode;
+  };
+}
